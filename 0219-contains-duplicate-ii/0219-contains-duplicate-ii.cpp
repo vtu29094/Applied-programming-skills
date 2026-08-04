4bd3be42df2e517c080cd2effdd95678135d68a3
vtu29094 <vtu29094@veltech.edu.in>
2026-08-04 15:01:51 +0530

    Time: 96 ms (36.74%), Space: 124.9 MB (11.42%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (mp.find(nums[i]) != mp.end()) {
+                if (i - mp[nums[i]] <= k)
+                    return true;
+            }
+            mp[nums[i]] = i; // Update the latest index
+        }
+
+        return false;
+    }
+};
